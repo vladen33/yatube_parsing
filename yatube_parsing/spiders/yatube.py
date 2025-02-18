cb7305845eb6472c5fc734419ffa389e3bf34c5e
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-18 22:05:05 +0300

    commit 567

--- yatube_parsing/spiders/yatube.py
+++ yatube_parsing/spiders/yatube.py
@@ -4,7 +4,20 @@ import scrapy
 class YatubeSpider(scrapy.Spider):
     name = "yatube"
     allowed_domains = ["158.160.177.221"]
-    start_urls = ["https://158.160.177.221"]
+    start_urls = ["http://158.160.177.221"]
 
     def parse(self, response):
-        pass
+        for post in response.css('div.card-body'):
+            yield {
+                'author': '@' + post.css("p.card-text a::attr(href)").get().strip('/'),
+                'text': ' '.join(post.css("p.card-text::text").getall()).strip(),
+                'date': post.css("small.text-muted::text").get()
+            }
+        next_page = response.css('ul.pagination').xpath('//li[last()]').css('a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+# В примерах записано так:
+# text = ' '.join(
+#     t.strip() for t in post.css('p::text').getall()
+# ).strip()
