217ea7e2c843993089006e9654e5c83d18d1b4a5
Vladislav Vasilev <vladen21@yandex.ru>
2025-02-19 23:19:36 +0300

    ertwert

--- yatube_parsing/spiders/group.py
+++ yatube_parsing/spiders/group.py
@@ -7,11 +7,18 @@ class GroupSpider(scrapy.Spider):
     start_urls = ["http://158.160.177.221/"]
 
     def parse(self, response):
+        # response.encoding = 'utf-8'
         all_groups = response.css('a[href^="/group/"]')
         for group_link in all_groups:
-            print('=======================', group_link)
-            yield response.follow(group_link)
+            yield response.follow(group_link, callback=self.parse_group)
 
         next_page = response.xpath('//a[contains(text(), "Следующая")]/@href').get()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
+
+    def parse_group(self, response):
+        yield {
+            'group_name': response.css('h2::text').get().strip(),
+            'description': response.css('p.group_descr::text').get().strip(),
+            'posts_count': int(response.css('div.posts_count::text').get().strip()[9:])
+        }
